4f52f7a790d80bd1d121f4e439d1ec3623b9630d
Fabian Fernandez <fabian.fernandez@gorillalogic.com>
2021-10-07 15:29:06 -0600

    Init project with ESLint and Prettier

---
+++ global.d.ts
@@ -0,0 +1,18 @@
+interface Global {
+  shallow: T
+  render: T
+  mount: T
+  shallow: T
+  render: T
+  noop: T
+  React: T
+  CONFIG: T
+}
+interface Window {
+  CONFIG: T
+  collections: T
+  queries: T
+  snapshots: T
+  onload: T
+  __REDUX_DEVTOOLS_EXTENSION__: T
+}
