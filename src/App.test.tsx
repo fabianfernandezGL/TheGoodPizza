cec329236c945c8d02cd93f39d1d72c1cbd9e389
Fabian Fernandez <fabian.fernandez@gorillalogic.com>
2021-10-13 19:25:20 -0600

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const msgElement = screen.getByText(/You have a new message/i)
+  expect(msgElement).toBeInTheDocument()
 })
