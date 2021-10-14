5954b11473226f03b14c5b4703a781f03824dae2
Fabian Fernandez <fabian.fernandez@gorillalogic.com>
2021-10-13 19:20:53 -0600

    Add CSS framework: Tailwind

---
+++ src/styles/theme.js
@@ -0,0 +1,37 @@
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: {
+        DEFAULT: '#FFF',
+      },
+      gray: {
+        dark: '#1F1F1F',
+        DEFAULT: '#333333',
+        light: '#4F4F4F',
+        lightest: '#BDBDBD',
+      },
+      yellow: {
+        DEFAULT: '#F2C94C',
+      },
+      black: {
+        DEFAULT: '#1F1F1F',
+        light: '#333333',
+      },
+      red: {
+        DEFAULT: '#EB5757',
+      },
+      green: {
+        dark: '#219653',
+        DEFAULT: '#219653',
+      },
+      orange: {
+        DEFAULT: '#F2994A',
+      },
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+    },
+  },
+}
