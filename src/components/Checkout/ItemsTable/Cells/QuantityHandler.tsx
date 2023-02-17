f2e7388b74f09e9a88ac449a95ef29ad9c64483a
Fabian Fernandez <fabian.fernandez@gorillalogic.com>
2023-02-17 15:01:02 -0600

    Add right icon btn component

--- src/components/Checkout/ItemsTable/Cells/QuantityHandler.tsx
+++ src/components/Checkout/ItemsTable/Cells/QuantityHandler.tsx
@@ -1,6 +1,6 @@
 import AddIcon from '@mui/icons-material/Add'
 import RemoveIcon from '@mui/icons-material/Remove'
-import { Stack, IconButton, Box, SxProps } from '@mui/material'
+import { Stack, Box, SxProps } from '@mui/material'
 
 import { theme } from 'styles/theme'
 import { useAppDispatch } from 'redux/hooks'
@@ -8,6 +8,7 @@ import { BigText } from 'components/Typography'
 import { removeItemQty, add } from 'redux/slices/cart'
 import { generateMenuItemData } from 'utils/cartHelper'
 import { PizzaInformation } from 'global.types'
+import IconButton from 'components/IconButton'
 
 const IconStyling: SxProps = {
   color: theme.colors.white.DEFAULT,
